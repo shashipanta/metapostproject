cd9d1b444ac1700968978b9c13eb0750b13538ff
samirzz <samirzz@pop-os.localdomain>
2023-05-09 21:11:03 +0545

    resolved

--- src/main/java/com/meta/userpostproject/controller/MainController.java
+++ src/main/java/com/meta/userpostproject/controller/MainController.java
@@ -5,18 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/uploaded_post_single_view")
-    public String singlePost(){
-        return "/uploaded_post_single_view";
+    @GetMapping("/")
+    public String loginPage(){
+        return "/login";
     }
-    @GetMapping("/create_table_view")
-    public String createPost(){
-        return "/create_table_view";
-    }
+//    @GetMapping("/register")
+//    public String registrationPage(){
+//        return "/registrationpage";
+  //  }
     @GetMapping("/post_uploaded")
-    public String postUpload(){
+    public String postPage(){
         return "/post_uploaded";
     }
+
+    @GetMapping("/create_table_view")
+    public String commentPage(){
+        return "/create_table_view";
+    }
+    @GetMapping("/uploaded_post_single_view")
+    public String singlepostPage(){
+        return "/uploaded_post_single_view";
+    }
+
 
 
 }
