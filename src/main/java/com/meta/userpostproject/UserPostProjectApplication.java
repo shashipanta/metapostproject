bffff2b4e4e1b7104b555974ae8f3cf40868dcf6
srijan-bhandari30 <np03a190286@heraldcollege.edu.np>
2023-05-15 19:09:08 +0545

    edited application.yml

--- src/main/java/com/meta/userpostproject/UserPostProjectApplication.java
+++ src/main/java/com/meta/userpostproject/UserPostProjectApplication.java
@@ -4,7 +4,6 @@ package com.meta.userpostproject;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,5 +15,7 @@ public class UserPostProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserPostProjectApplication.class, args);
     }
+
+
 
 }
