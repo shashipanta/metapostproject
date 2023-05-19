9b3e7d516e8fe86fa270a9785e30d608f9d8eed2
shashipanta <shashipanta57@gmail.com>
2023-05-19 08:47:32 +0545

    config:Added config to load static file from src directly

--- src/main/java/com/meta/userpostproject/controller/DefaultController.java
+++ src/main/java/com/meta/userpostproject/controller/DefaultController.java
@@ -1,19 +1,49 @@
 package com.meta.userpostproject.controller;
 
+import com.meta.userpostproject.dto.PostDto;
+import com.meta.userpostproject.dto.PostTableViewDto;
+import com.meta.userpostproject.model.Post;
+import com.meta.userpostproject.service.PostService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class DefaultController {
+
+    private final PostService postService;
+
+    public DefaultController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping()
     public String redirect(){
         return "/internal/postActionPage";
     }
-    @GetMapping("/decidePost")
-    public String adminPostDecide(){
+    @GetMapping("/approve-post")
+    public String adminPostDecide(Model model){
+
+
+        List<PostTableViewDto> posts = postService.getAllPosts();
+
+        model.addAttribute("posts", posts);
         return "/internal/postApprovalPage";
+    }
+
+    @GetMapping("/view-post/{id}")
+    public String reviewSinglePostForApproval(@PathVariable(name = "id") Short postId, Model model){
+
+        PostDto postDto = postService.getSinglePost(postId);
+
+        model.addAttribute("postDto", postDto);
+        return "/internal/single-post-view";
     }
 
 
